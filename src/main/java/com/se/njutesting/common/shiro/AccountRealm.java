9b33b5f489bc8c85c05dbcf34b8ce1fa402160cf
solitarynew <1348288404@qq.com>
2022-04-04 22:06:10 +0800

    添加Shiro基本认证与授权框架

---
+++ src/main/java/com/se/njutesting/common/shiro/AccountRealm.java
@@ -0,0 +1,43 @@
+package com.se.njutesting.common.shiro;
+
+import com.se.njutesting.common.util.JwtUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class AccountRealm extends AuthorizingRealm {
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @Override
+    public boolean supports(AuthenticationToken token) {
+        return token instanceof JwtToken;
+    }
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
+        info.addRole("user");
+        info.addStringPermission("user:access");
+        return info;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        JwtToken jwtToken = (JwtToken) authenticationToken;
+        String userId = (String) jwtUtils.getClaimByToken((String) jwtToken.getPrincipal()).get("userId");
+        String username = (String) jwtUtils.getClaimByToken((String) jwtToken.getPrincipal()).get("username");
+        //TODO:验证用户
+        return new SimpleAuthenticationInfo(null, jwtToken.getCredentials(), getName());
+    }
+}
