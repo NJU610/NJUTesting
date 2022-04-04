e5f8bdad10667f4a3ccdf3bce78df5c2e65a65e0
hollowopk <1808479906@qq.com>
2022-04-04 22:12:04 +0800

    fix bug

--- src/main/java/com/se/njutesting/module/service/impl/UserServiceImpl.java
+++ src/main/java/com/se/njutesting/module/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.se.njutesting.module.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.se.njutesting.module.dao.UserMapper;
 import com.se.njutesting.module.entity.User;
@@ -15,11 +16,13 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
 
     @Override
     public List<User> selectUserByName(String username) {
-        return this.baseMapper.selectUserByName(username);
+        QueryWrapper<User> wrapper = new QueryWrapper<>();
+        wrapper.eq("username", username);
+        return baseMapper.selectList(wrapper);
     }
 
     @Override
     public List<User> selectAllUsers() {
-        return this.baseMapper.selectAllUsers();
+        return this.baseMapper.selectList(null);
     }
 }
