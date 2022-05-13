5d273d136a3655942c4862d3b216ea3255f2026a
hollowopk <1808479906@qq.com>
2022-05-13 15:52:39 +0800

    完善测试报告接口文档

--- yudao-module-system/yudao-module-system-impl/src/main/java/cn/iocoder/yudao/module/system/controller/admin/report/ReportController.java
+++ yudao-module-system/yudao-module-system-impl/src/main/java/cn/iocoder/yudao/module/system/controller/admin/report/ReportController.java
@@ -36,118 +36,130 @@ public class ReportController {
     private ReportService reportService;
 
     @PostMapping("/create")
-    @ApiOperation("测试部人员-创建测试报告")
+    @ApiOperation(value = "测试部人员-创建测试报告", notes = "需要填写delegationId字段。其中delegationId为委托编号。返回值为测试报告编号。")
     public CommonResult<Long> createReport(@Valid @RequestBody ReportCreateReqVO createReqVO) {
         return success(reportService.createReport(createReqVO));
     }
 
     @PutMapping("/save/table7")
-    @ApiOperation("保存软件测试报告")
+    @ApiOperation(value = "保存软件测试报告",
+            notes = "需要填写reportId和data字段，其中reportId为测试报告编号，data是json格式，包含表格内容。返回值为是否保存成功。")
     public CommonResult<Boolean> saveReportTable7(@Valid @RequestBody ReportSaveTableReqVO saveReqVO) {
         reportService.saveReportTable7(saveReqVO);
         return success(true);
     }
 
     @PutMapping("/save/table8")
-    @ApiOperation("保存测试用例（电子记录）")
+    @ApiOperation(value = "保存测试用例（电子记录）",
+            notes = "需要填写reportId和data字段，其中reportId为测试报告编号，data是json格式，包含表格内容。返回值为是否保存成功。")
     public CommonResult<Boolean> saveReportTable8(@Valid @RequestBody ReportSaveTableReqVO saveReqVO) {
         reportService.saveReportTable8(saveReqVO);
         return success(true);
     }
 
     @PutMapping("/save/table9")
-    @ApiOperation("保存软件测试记录（电子记录）")
+    @ApiOperation(value = "保存软件测试记录（电子记录）",
+            notes = "需要填写reportId和data字段，其中reportId为测试报告编号，data是json格式，包含表格内容。返回值为是否保存成功。")
     public CommonResult<Boolean> saveReportTable9(@Valid @RequestBody ReportSaveTableReqVO saveReqVO) {
         reportService.saveReportTable9(saveReqVO);
         return success(true);
     }
 
     @PutMapping("/save/table10")
-    @ApiOperation("保存测试报告检查表")
+    @ApiOperation(value = "保存测试报告检查表",
+            notes = "需要填写reportId和data字段，其中reportId为测试报告编号，data是json格式，包含表格内容。返回值为是否保存成功。")
     public CommonResult<Boolean> saveReportTable10(@Valid @RequestBody ReportSaveTableReqVO saveReqVO) {
         reportService.saveReportTable10(saveReqVO);
         return success(true);
     }
 
     @PutMapping("/save/table11")
-    @ApiOperation("保存软件测试问题清单（电子记录）")
+    @ApiOperation(value = "保存软件测试问题清单（电子记录）",
+            notes = "需要填写reportId和data字段，其中reportId为测试报告编号，data是json格式，包含表格内容。返回值为是否保存成功。")
     public CommonResult<Boolean> saveReportTable11(@Valid @RequestBody ReportSaveTableReqVO saveReqVO) {
         reportService.saveReportTable11(saveReqVO);
         return success(true);
     }
 
     @PutMapping("/submit")
-    @ApiOperation("测试部人员-提交测试报告")
+    @ApiOperation(value = "测试部人员-提交测试报告",
+            notes = "需要填写id字段。其中id为测试报告编号。返回值为是否提交成功。")
     public CommonResult<Boolean> submitReport(@Valid @RequestBody ReportSubmitReqVO submitReqVO) {
         reportService.submitReport(submitReqVO);
         return success(true);
     }
 
     @PutMapping("/accept/manager")
-    @ApiOperation("测试部主管-审核测试报告通过")
+    @ApiOperation(value = "测试部主管-审核测试报告通过",
+            notes = "需要填写id字段，选填remark字段。其中id为测试报告编号，remark为审核意见。返回值为是否提交成功。")
     public CommonResult<Boolean> acceptReportManager(@Valid @RequestBody ReportAcceptReqVO acceptReqVO) {
         reportService.acceptReportManager(acceptReqVO);
         return success(true);
     }
 
     @PutMapping("/reject/manager")
-    @ApiOperation("测试部主管-审核测试报告不通过")
+    @ApiOperation(value = "测试部主管-审核测试报告不通过",
+            notes = "需要填写id和remark字段。其中id为测试报告编号，remark为审核不通过原因。返回值为是否提交成功。")
     public CommonResult<Boolean> rejectReportManager(@Valid @RequestBody ReportRejectReqVO rejectReqVO) {
         reportService.rejectReportManager(rejectReqVO);
         return success(true);
     }
 
     @PutMapping("/accept/client")
-    @ApiOperation("客户-审核测试报告通过")
+    @ApiOperation(value = "客户-审核测试报告通过",
+            notes = "需要填写id字段，选填remark字段。其中id为测试报告编号，remark为审核意见。返回值为是否提交成功。")
     public CommonResult<Boolean> acceptReportClient(@Valid @RequestBody ReportAcceptReqVO acceptReqVO) {
         reportService.acceptReportClient(acceptReqVO);
         return success(true);
     }
 
     @PutMapping("/reject/client")
-    @ApiOperation("客户-审核测试报告不通过")
+    @ApiOperation(value = "客户-审核测试报告不通过",
+            notes = "需要填写id和remark字段。其中id为测试报告编号，remark为审核不通过原因。返回值为是否提交成功。")
     public CommonResult<Boolean> rejectReportClient(@Valid @RequestBody ReportRejectReqVO rejectReqVO) {
         reportService.rejectReportClient(rejectReqVO);
         return success(true);
     }
 
     @PutMapping("/accept/signatory")
-    @ApiOperation("授权签字人-审核测试报告通过")
+    @ApiOperation(value = "授权签字人-审核测试报告通过",
+            notes = "需要填写id字段，选填remark字段。其中id为测试报告编号，remark为审核意见。返回值为是否提交成功。")
     public CommonResult<Boolean> acceptReportSignatory(@Valid @RequestBody ReportAcceptReqVO acceptReqVO) {
         reportService.acceptReportSignatory(acceptReqVO);
         return success(true);
     }
 
     @PutMapping("/reject/signatory")
-    @ApiOperation("授权签字人-审核测试报告不通过")
+    @ApiOperation(value = "授权签字人-审核测试报告不通过",
+            notes = "需要填写id和remark字段。其中id为测试报告编号，remark为审核不通过原因。返回值为是否提交成功。")
     public CommonResult<Boolean> rejectReportSignatory(@Valid @RequestBody ReportRejectReqVO rejectReqVO) {
         reportService.rejectReportSignatory(rejectReqVO);
         return success(true);
     }
 
     @PutMapping("/archive")
-    @ApiOperation("测试部-归档测试报告")
+    @ApiOperation(value = "测试部-归档测试报告", notes = "需要填写id字段。其中id为测试报告编号。返回值为是否更新成功。")
     public CommonResult<Boolean> archiveReport(@Valid @RequestBody ReportArchiveReqVO archiveReqVO) {
         reportService.archiveReport(archiveReqVO);
         return success(true);
     }
 
     @PutMapping("/send")
-    @ApiOperation("市场部-发送测试报告")
+    @ApiOperation(value = "市场部-发送测试报告", notes = "需要填写id字段。其中id为测试报告编号。返回值为是否发送成功。")
     public CommonResult<Boolean> sendReport(@Valid @RequestBody ReportSendReqVO sendReqVO) {
         reportService.sendReport(sendReqVO);
         return success(true);
     }
 
     @PutMapping("/receive")
-    @ApiOperation("客户-确认接收测试报告")
+    @ApiOperation(value = "客户-确认接收测试报告", notes = "需要填写id字段。其中id为测试报告编号。返回值为是否确认成功。")
     public CommonResult<Boolean> receiveReport(@Valid @RequestBody ReportReceiveReqVO receiveReqVO) {
         reportService.receiveReport(receiveReqVO);
         return success(true);
     }
 
     @GetMapping("/get")
-    @ApiOperation("获得测试报告")
+    @ApiOperation(value = "获得测试报告", notes = "需要填写id字段。其中id为测试报告编号。返回值为测试报告信息。")
     @ApiImplicitParam(name = "id", value = "编号", required = true, example = "1024", dataTypeClass = Long.class)
     public CommonResult<ReportRespVO> getReport(@RequestParam("id") Long id) {
         ReportDO report = reportService.getReport(id);
@@ -155,42 +167,47 @@ public class ReportController {
     }
 
     @GetMapping("/get/table7")
-    @ApiOperation("获得软件测试报告")
+    @ApiOperation(value = "获得软件测试报告",
+            notes = "需要填写id字段。其中id为表格编号，从测试报告的返回值中获取。返回值为json格式，存放在data字段中，包含表格内容。")
     @ApiImplicitParam(name = "id", value = "编号", required = true, example = "1024", dataTypeClass = String.class)
     public CommonResult<String> getReportTable7(@RequestParam("id") String id) {
         return success(reportService.getReportTable("table7", id));
     }
 
     @GetMapping("/get/table8")
-    @ApiOperation("获得测试用例（电子记录）")
+    @ApiOperation(value = "获得测试用例（电子记录）",
+            notes = "需要填写id字段。其中id为表格编号，从测试报告的返回值中获取。返回值为json格式，存放在data字段中，包含表格内容。")
     @ApiImplicitParam(name = "id", value = "编号", required = true, example = "1024", dataTypeClass = String.class)
     public CommonResult<String> getReportTable8(@RequestParam("id") String id) {
         return success(reportService.getReportTable("table8", id));
     }
 
     @GetMapping("/get/table9")
-    @ApiOperation("获得软件测试记录（电子记录）")
+    @ApiOperation(value = "获得软件测试记录（电子记录）",
+            notes = "需要填写id字段。其中id为表格编号，从测试报告的返回值中获取。返回值为json格式，存放在data字段中，包含表格内容。")
     @ApiImplicitParam(name = "id", value = "编号", required = true, example = "1024", dataTypeClass = String.class)
     public CommonResult<String> getReportTable9(@RequestParam("id") String id) {
         return success(reportService.getReportTable("table9", id));
     }
 
     @GetMapping("/get/table10")
-    @ApiOperation("获得测试报告检查表")
+    @ApiOperation(value = "获得测试报告检查表",
+            notes = "需要填写id字段。其中id为表格编号，从测试报告的返回值中获取。返回值为json格式，存放在data字段中，包含表格内容。")
     @ApiImplicitParam(name = "id", value = "编号", required = true, example = "1024", dataTypeClass = String.class)
     public CommonResult<String> getReportTable10(@RequestParam("id") String id) {
         return success(reportService.getReportTable("table10", id));
     }
 
     @GetMapping("/get/table11")
-    @ApiOperation("获得软件测试问题清单（电子记录）")
+    @ApiOperation(value = "获得软件测试问题清单（电子记录）",
+            notes = "需要填写id字段。其中id为表格编号，从测试报告的返回值中获取。返回值为json格式，存放在data字段中，包含表格内容。")
     @ApiImplicitParam(name = "id", value = "编号", required = true, example = "1024", dataTypeClass = String.class)
     public CommonResult<String> getReportTable11(@RequestParam("id") String id) {
         return success(reportService.getReportTable("table11", id));
     }
 
     @GetMapping("/list")
-    @ApiOperation("获得测试报告列表")
+    @ApiOperation(value = "获得测试报告列表", notes = "根据需要填写多个测试报告id，获得所有测试报告。返回值为测试报告列表。")
     @ApiImplicitParam(name = "ids", value = "编号列表", required = true, example = "1024,2048", dataTypeClass = List.class)
     public CommonResult<List<ReportRespVO>> getReportList(@RequestParam("ids") Collection<Long> ids) {
         List<ReportDO> list = reportService.getReportList(ids);
@@ -198,7 +215,8 @@ public class ReportController {
     }
 
     @GetMapping("/page")
-    @ApiOperation("获得测试报告分页")
+    @ApiOperation(value = "获得测试报告分页",
+            notes = "需要填写页码pageNo和每页条数pageSize，再根据需要填写其他查询字段。返回值为测试报告分页。")
     public CommonResult<PageResult<ReportRespVO>> getReportPage(@Valid ReportPageReqVO pageVO) {
         PageResult<ReportDO> pageResult = reportService.getReportPage(pageVO);
         return success(ReportConvert.INSTANCE.convertPage(pageResult));
