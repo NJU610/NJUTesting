c13c6166c2bf51847aa91f10fd4925e78c6f04d2
hollowopk <1808479906@qq.com>
2022-05-27 13:48:38 +0800

    新增根据不同条件排序查询委托分页

--- yudao-module-system/yudao-module-system-impl/src/main/java/cn/iocoder/yudao/module/system/dal/mysql/delegation/DelegationMapper.java
+++ yudao-module-system/yudao-module-system-impl/src/main/java/cn/iocoder/yudao/module/system/dal/mysql/delegation/DelegationMapper.java
@@ -1,17 +1,16 @@
 package cn.iocoder.yudao.module.system.dal.mysql.delegation;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 import cn.iocoder.yudao.framework.common.pojo.PageResult;
-import cn.iocoder.yudao.framework.mybatis.core.mapper.BaseMapperX;
 import cn.iocoder.yudao.framework.mybatis.core.query.LambdaQueryWrapperX;
-import cn.iocoder.yudao.module.system.controller.admin.delegation.vo.DelegationExportReqVO;
-import cn.iocoder.yudao.module.system.controller.admin.delegation.vo.DelegationPageReqVO;
+import cn.iocoder.yudao.framework.mybatis.core.mapper.BaseMapperX;
+import cn.iocoder.yudao.framework.mybatis.core.query.QueryWrapperX;
 import cn.iocoder.yudao.module.system.dal.dataobject.delegation.DelegationDO;
 import cn.iocoder.yudao.module.system.enums.delegation.DelegationStateEnum;
 import org.apache.ibatis.annotations.Mapper;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import cn.iocoder.yudao.module.system.controller.admin.delegation.vo.*;
 
 import static cn.iocoder.yudao.framework.common.exception.util.ServiceExceptionUtil.exception;
 import static cn.iocoder.yudao.module.system.enums.ErrorCodeConstants.*;
@@ -25,23 +24,34 @@ import static cn.iocoder.yudao.module.system.enums.ErrorCodeConstants.*;
 public interface DelegationMapper extends BaseMapperX<DelegationDO> {
 
     default PageResult<DelegationDO> selectPage(DelegationPageReqVO reqVO) {
-        return selectPage(reqVO, new LambdaQueryWrapperX<DelegationDO>()
-                .eqIfPresent(DelegationDO::getCreatorId, reqVO.getCreatorId())
-                .betweenIfPresent(DelegationDO::getLaunchTime, reqVO.getBeginLaunchTime(), reqVO.getEndLaunchTime())
-                .likeIfPresent(DelegationDO::getName, reqVO.getName())
-                .eqIfPresent(DelegationDO::getTable2Id, reqVO.getTable2Id())
-                .eqIfPresent(DelegationDO::getTable3Id, reqVO.getTable3Id())
-                .eqIfPresent(DelegationDO::getMarketDeptStaffId, reqVO.getMarketDeptStaffId())
-                .eqIfPresent(DelegationDO::getTestingDeptStaffId, reqVO.getTestingDeptStaffId())
-                .eqIfPresent(DelegationDO::getTable14Id, reqVO.getTable14Id())
-                .eqIfPresent(DelegationDO::getOfferId, reqVO.getOfferId())
-                .eqIfPresent(DelegationDO::getContractId, reqVO.getContractId())
-                .eqIfPresent(DelegationDO::getSampleId, reqVO.getSampleId())
-                .eqIfPresent(DelegationDO::getSolutionId, reqVO.getSolutionId())
-                .eqIfPresent(DelegationDO::getReportId, reqVO.getReportId())
-                .inIfPresent(DelegationDO::getState, reqVO.getState())
-                .betweenIfPresent(DelegationDO::getCreateTime, reqVO.getBeginCreateTime(), reqVO.getEndCreateTime())
-                .orderByDesc(DelegationDO::getId));
+        if (reqVO.getOrderField() == null) {
+            reqVO.setOrderField("id");
+        }
+        if (reqVO.getAsc() == null) {
+            reqVO.setAsc(true);
+        }
+        QueryWrapperX<DelegationDO> queryWrapper = new QueryWrapperX<DelegationDO>()
+                .eqIfPresent("creator_id", reqVO.getCreatorId())
+                .betweenIfPresent("launch_time", reqVO.getBeginLaunchTime(), reqVO.getEndLaunchTime())
+                .likeIfPresent("name", reqVO.getName())
+                .eqIfPresent("table2_id", reqVO.getTable2Id())
+                .eqIfPresent("table3_id", reqVO.getTable3Id())
+                .eqIfPresent("market_dept_staff_id", reqVO.getMarketDeptStaffId())
+                .eqIfPresent("testing_dept_staff_id", reqVO.getTestingDeptStaffId())
+                .eqIfPresent("table14_id", reqVO.getTable14Id())
+                .eqIfPresent("offer_id", reqVO.getOfferId())
+                .eqIfPresent("contract_id", reqVO.getContractId())
+                .eqIfPresent("sample_id", reqVO.getSampleId())
+                .eqIfPresent("solution_id", reqVO.getSolutionId())
+                .eqIfPresent("report_id", reqVO.getReportId())
+                .inIfPresent("state", reqVO.getState())
+                .betweenIfPresent("create_time", reqVO.getBeginCreateTime(), reqVO.getEndCreateTime());
+        if (reqVO.getAsc()) {
+            queryWrapper.orderByAsc(reqVO.getOrderField());
+        } else {
+            queryWrapper.orderByDesc(reqVO.getOrderField());
+        }
+        return selectPage(reqVO, queryWrapper);
     }
 
     default List<DelegationDO> selectList(DelegationExportReqVO reqVO) {
