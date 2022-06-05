93ef465d9cf77624e3a21fe4e54e07ce05c62d1e
Hattrick <693901492@qq.com>
2022-06-05 18:48:01 +0800

    doc file

--- yudao-server/src/main/resources/tool/JS013/test.py
+++ yudao-server/src/main/resources/tool/JS013/test.py
@@ -6,62 +6,77 @@ import xlwt
 from docx2pdf import convert
 from mailmerge import MailMerge  # 引用邮件处理模块
 
-# datafile_path = 'C:\\Users\\Yongp\\Desktop\\Classified\\contract_word\\js002\\js002.xlsx'  # 表格位置
-# data = xlrd.open_workbook(datafile_path)
-# table = data.sheet_by_name('这是一个合同')  # excel表名
-template = 'C:\\Users\\Yongp\\Desktop\\Classified\\contract_word\\js013\\NST－04－JS013－2011 - 测试方案评审表.docx'  # 模版位置
-# nrows = table.nrows
-with open("C:\\Users\\Yongp\\Desktop\\Classified\\contract_word\\js013\\data.json", 'r', encoding='utf8') as fp:
-  jdata = json.load(fp)
-  
-  document = MailMerge(template)
 
-  document.merge(
-    SoftwareName = jdata["softName"],
-    SoftwareVer = jdata["version"],
-    ProjectID = jdata["contractId"],
-    TestType = jdata["type"],
+def main(argv):
+      
+  o_path = ""
+  i_path = ""
+  template = ""
+  
+  try:
+    opts, args = getopt.getopt(argv,'o:i:t:',["ofile=","ifile=","template="])
+  except getopt.GetoptError:
+    sys.exit(2)
+  
+  for opt, arg in opts:
+      if opt in ("-o", "--ofile"):
+          o_path = arg
+      elif opt in ("-i", "--ifile"):
+          i_path = arg 
+      elif opt in ("-t", "--template"):
+          template = arg  
+  with open(i_path, 'r', encoding='utf8') as fp:
+    jdata = json.load(fp)
     
-    State1 = jdata["result"][0]["state"],
-    State2 = jdata["result"][1]["state"],
-    State3 = jdata["result"][2]["state"],
-    State4 = jdata["result"][3]["state"],
-    State5 = jdata["result"][4]["state"],
-    State6 = jdata["result"][5]["state"],
-    State7 = jdata["result"][6]["state"],
-    State8 = jdata["result"][7]["state"],
-    
-    Reason1 = jdata["result"][0]["reason"],
-    Reason2 = jdata["result"][1]["reason"],
-    Reason3 = jdata["result"][2]["reason"],
-    Reason4 = jdata["result"][3]["reason"],
-    Reason5 = jdata["result"][4]["reason"],
-    Reason6 = jdata["result"][5]["reason"],
-    Reason7 = jdata["result"][6]["reason"],
-    Reason8 = jdata["result"][7]["reason"],
-    
-    Advice1 = jdata["advice"][0]["advice"],
-    Advice2 = jdata["advice"][1]["advice"],
-    Advice3 = jdata["advice"][2]["advice"],
-    Advice4 = jdata["advice"][3]["advice"],
-    Advice5 = jdata["advice"][4]["advice"],
-    
-    Sig1 = jdata["advice"][0]["signature"],
-    Sig2 = jdata["advice"][1]["signature"],
-    Sig3 = jdata["advice"][2]["signature"],
-    Sig4 = jdata["advice"][3]["signature"],
-    Sig5 = jdata["advice"][4]["signature"],
-    
-    Date1 = jdata["advice"][0]["date"],
-    Date2 = jdata["advice"][1]["date"],
-    Date3 = jdata["advice"][2]["date"],
-    Date4 = jdata["advice"][3]["date"],
-    Date5 = jdata["advice"][4]["date"],
-  )
-  o_path = 'C:\\Users\\Yongp\\Desktop\\Classified\\contract_word\\js013\\' #输出地址
-  o_name = jdata["_id"] + "_" + "JS013"
-  wordname = o_path + o_name + '.docx' 
-  document.write(wordname)  # 创建新文件
-  convert(wordname, o_path + o_name + '.pdf')
+    document = MailMerge(template)
+
+    document.merge(
+      SoftwareName = jdata["softName"],
+      SoftwareVer = jdata["version"],
+      ProjectID = jdata["contractId"],
+      TestType = jdata["type"],
+      
+      State1 = jdata["result"][0]["state"],
+      State2 = jdata["result"][1]["state"],
+      State3 = jdata["result"][2]["state"],
+      State4 = jdata["result"][3]["state"],
+      State5 = jdata["result"][4]["state"],
+      State6 = jdata["result"][5]["state"],
+      State7 = jdata["result"][6]["state"],
+      State8 = jdata["result"][7]["state"],
+      
+      Reason1 = jdata["result"][0]["reason"],
+      Reason2 = jdata["result"][1]["reason"],
+      Reason3 = jdata["result"][2]["reason"],
+      Reason4 = jdata["result"][3]["reason"],
+      Reason5 = jdata["result"][4]["reason"],
+      Reason6 = jdata["result"][5]["reason"],
+      Reason7 = jdata["result"][6]["reason"],
+      Reason8 = jdata["result"][7]["reason"],
+      
+      Advice1 = jdata["advice"][0]["advice"],
+      Advice2 = jdata["advice"][1]["advice"],
+      Advice3 = jdata["advice"][2]["advice"],
+      Advice4 = jdata["advice"][3]["advice"],
+      Advice5 = jdata["advice"][4]["advice"],
+      
+      Sig1 = jdata["advice"][0]["signature"],
+      Sig2 = jdata["advice"][1]["signature"],
+      Sig3 = jdata["advice"][2]["signature"],
+      Sig4 = jdata["advice"][3]["signature"],
+      Sig5 = jdata["advice"][4]["signature"],
+      
+      Date1 = jdata["advice"][0]["date"],
+      Date2 = jdata["advice"][1]["date"],
+      Date3 = jdata["advice"][2]["date"],
+      Date4 = jdata["advice"][3]["date"],
+      Date5 = jdata["advice"][4]["date"],
+    )
+    wordname = o_path + '.docx' 
+    document.write(wordname)  # 创建新文件
+    convert(wordname, o_path + '.pdf')
       
     
+    
+if __name__ == '__main__':
+    main(sys.argv[1:])
--- yudao-server/src/main/resources/tool/offer/test.py
+++ yudao-server/src/main/resources/tool/offer/test.py
@@ -4,44 +4,60 @@ from tkinter import PROJECTING
 import xlrd  # 引入Excel读取模块
 import json
 import xlwt
+import getopt
+import sys
 from docx2pdf import convert
 from mailmerge import MailMerge  # 引用邮件处理模块
 
-# datafile_path = 'C:\\Users\\Yongp\\Desktop\\Classified\\contract_word\\js002\\js002.xlsx'  # 表格位置
-# data = xlrd.open_workbook(datafile_path)
-# table = data.sheet_by_name('这是一个合同')  # excel表名
-template = 'C:\\Users\\Yongp\\Desktop\\Classified\\contract_word\\offer\\报价单.docx'  # 模版位置
-# nrows = table.nrows
-
-with open("C:\\Users\\Yongp\\Desktop\\Classified\\contract_word\\offer\\data.json", 'r', encoding='utf8') as fp:
-  jdata = json.load(fp)
-  document = MailMerge(template)
-
-  document.merge(
-    Project2 = "",
-    Item2 = "",
-    Price2 = "",
-    Brief2 = "",
-    LTotal2 = "",
+def main(argv):
+      
+  o_path = ""
+  i_path = ""
+  template = ""
+  
+  try:
+    opts, args = getopt.getopt(argv,'o:i:t:',["ofile=","ifile=","template="])
+  except getopt.GetoptError:
+    sys.exit(2)
     
-    Privider = jdata["报价提供人"],
-    Date = jdata["报价日期"],
-    StartDate = jdata["报价有效期"][0],
-    EndDate = jdata["报价有效期"][1],
-    Subtotal = jdata["小计"],
-    Total = jdata["总计"],
-    Tax = jdata["税率（8%）"],
-    
-    Project1 = jdata["项目表格"][0]["xiangmu"],
-    Item1 = jdata["项目表格"][0]["fenxiang"],
-    Price1 = jdata["项目表格"][0]["danjia"],
-    Brief1 = jdata["项目表格"][0]["shuoming"],
-    LTotal1 = jdata["项目表格"][0]["hangheji"],
-  )
-  o_path = 'C:\\Users\\Yongp\\Desktop\\Classified\\contract_word\\offer\\' #输出地址
-  o_name = jdata["_id"] + "_" + "JS014"
-  wordname = o_path + o_name + '.docx' 
-  document.write(wordname)  # 创建新文件
-  convert(wordname, o_path + o_name + '.pdf')
+  for opt, arg in opts:
+      if opt in ("-o", "--ofile"):
+          o_path = arg
+      elif opt in ("-i", "--ifile"):
+          i_path = arg 
+      elif opt in ("-t", "--template"):
+          template = arg
+  with open(i_path, 'r', encoding='utf8') as fp:
+    jdata = json.load(fp)
+    document = MailMerge(template)
+
+    document.merge(
+      Project2 = "",
+      Item2 = "",
+      Price2 = "",
+      Brief2 = "",
+      LTotal2 = "",
+      
+      Privider = jdata["报价提供人"],
+      Date = jdata["报价日期"],
+      StartDate = jdata["报价有效期"][0],
+      EndDate = jdata["报价有效期"][1],
+      Subtotal = jdata["小计"],
+      Total = jdata["总计"],
+      Tax = jdata["税率（8%）"],
+      
+      Project1 = jdata["项目表格"][0]["xiangmu"],
+      Item1 = jdata["项目表格"][0]["fenxiang"],
+      Price1 = jdata["项目表格"][0]["danjia"],
+      Brief1 = jdata["项目表格"][0]["shuoming"],
+      LTotal1 = jdata["项目表格"][0]["hangheji"],
+    )
+    wordname = o_path + '.docx' 
+    document.write(wordname)  # 创建新文件
+    convert(wordname, o_path + '.pdf')
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
       
     
