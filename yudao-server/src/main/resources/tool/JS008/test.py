e0c97edcc54e432b906f1062113ea57c62e8a1b5
Hattrick <693901492@qq.com>
2022-06-05 18:54:52 +0800

    add xls

--- yudao-server/src/main/resources/tool/JS008/test.py
+++ yudao-server/src/main/resources/tool/JS008/test.py
@@ -1,8 +1,29 @@
 import json
 import xlrd
 import xlwt
+import getopt
+import sys
 
-with open("C:\\Users\\Yongp\\Desktop\\Classified\\contract_word\\js008\\data.json", 'r', encoding='utf8') as fp:
+def main(argv):
+      
+  o_path = ""
+  i_path = ""
+  template = ""
+  
+  try:
+    opts, args = getopt.getopt(argv,'o:i:t',["ofile=","ifile=","template="])
+  except getopt.GetoptError:
+    sys.exit(2)
+    
+  for opt, arg in opts:
+      if opt in ("-o", "--ofile"):
+          o_path = arg
+      elif opt in ("-i", "--ifile"):
+          i_path = arg 
+      elif opt in ("-t", "--template"):
+          template = arg
+
+  with open(i_path, 'r', encoding='utf8') as fp:
     jdata = json.load(fp)
     data = jdata["测试用例"]
     xl = xlwt.Workbook(encoding='utf-8')
@@ -31,6 +52,7 @@ with open("C:\\Users\\Yongp\\Desktop\\Classified\\contract_word\\js008\\data.jso
         sheet.write(cnt, 5, i["author"])
         sheet.write(cnt, 6, i["time"])
     
-    xl.save("C:\\Users\\Yongp\\Desktop\\Classified\\contract_word\\js008\\output.xls")
+    xl.save(o_path + ".xls")
     
-    
+if __name__ == '__main__':
+    main(sys.argv[1:])
