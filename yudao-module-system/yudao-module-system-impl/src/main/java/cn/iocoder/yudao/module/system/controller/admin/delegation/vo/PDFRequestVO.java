6e8a99b21eeb921d4f140bb012582443e8c28958
solitarynew <1348288404@qq.com>
2022-06-05 18:32:02 +0800

    新增PDFRequestVO

---
+++ yudao-module-system/yudao-module-system-impl/src/main/java/cn/iocoder/yudao/module/system/controller/admin/delegation/vo/PDFRequestVO.java
@@ -0,0 +1,25 @@
+package cn.iocoder.yudao.module.system.controller.admin.delegation.vo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import javax.validation.constraints.NotNull;
+
+@ApiModel("生成PDF Request VO")
+@Data
+@EqualsAndHashCode
+@ToString
+public class PDFRequestVO {
+
+    @ApiModelProperty(value = "表格在MongoDB中的主键编号", example = "627cb1baffb4134949aa10c8", required = true)
+    @NotNull(message = "表格编号不能为空")
+    String tableId;
+
+    @ApiModelProperty(value = "table的名称，例如一般表格为填写table10，标价单为offer", example = "table10", required = true)
+    @NotNull(message = "table的名称不能为空")
+    String tableName;
+
+}
