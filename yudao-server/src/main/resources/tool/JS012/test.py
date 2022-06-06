a6bfeb8e7f277d2e9f6a2f3cad463f5f798ded91
Hattrick <693901492@qq.com>
2022-06-06 20:09:19 +0800

    update all module

--- yudao-server/src/main/resources/tool/JS012/test.py
+++ yudao-server/src/main/resources/tool/JS012/test.py
@@ -34,25 +34,7 @@ def main(argv):
     document = MailMerge(template)
 
     document.merge(
-      Del = jdata["委托单位"],
-      SoftName = jdata["软件名称"],
-      Reviewer = jdata["检查人"],
-      Date = jdata["日期"],
-      
-      Res1 = jdata["dataSource"][0]["result"],
-      Res2 = jdata["dataSource"][1]["result"],
-      Res3 = jdata["dataSource"][2]["result"],
-      Res4 = jdata["dataSource"][3]["result"],
-      Res5 = jdata["dataSource"][4]["result"],
-      Res6 = jdata["dataSource"][5]["result"],
-      Res7 = jdata["dataSource"][6]["result"],
-      Res8 = jdata["dataSource"][7]["result"],
-      Res9 = jdata["dataSource"][8]["result"],
-      Res10 = jdata["dataSource"][9]["result"],
-      Res11 = jdata["dataSource"][10]["result"],
-      Res12 = jdata["dataSource"][11]["result"],
-      Res13 = jdata["dataSource"][12]["result"],
-      Res14 = jdata["dataSource"][13]["result"],
+
     )
 
     wordname = o_path + '.docx' 
