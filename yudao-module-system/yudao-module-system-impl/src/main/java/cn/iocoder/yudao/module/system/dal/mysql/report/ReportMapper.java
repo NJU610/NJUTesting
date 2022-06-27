48f43fef70762f122190168735855a4a84a910bd
Andres-Nju <1572378310@qq.com>
2022-06-27 18:42:46 +0800

    用户公司关联和测试报告的mapper添加

--- yudao-module-system/yudao-module-system-impl/src/main/java/cn/iocoder/yudao/module/system/dal/mysql/report/ReportMapper.java
+++ yudao-module-system/yudao-module-system-impl/src/main/java/cn/iocoder/yudao/module/system/dal/mysql/report/ReportMapper.java
@@ -5,6 +5,7 @@ import java.util.*;
 import cn.iocoder.yudao.framework.common.pojo.PageResult;
 import cn.iocoder.yudao.framework.mybatis.core.query.LambdaQueryWrapperX;
 import cn.iocoder.yudao.framework.mybatis.core.mapper.BaseMapperX;
+import cn.iocoder.yudao.module.system.dal.dataobject.company.UserCompanyDO;
 import cn.iocoder.yudao.module.system.dal.dataobject.report.ReportDO;
 import org.apache.ibatis.annotations.Mapper;
 import cn.iocoder.yudao.module.system.controller.admin.report.vo.*;
@@ -16,6 +17,43 @@ import cn.iocoder.yudao.module.system.controller.admin.report.vo.*;
  */
 @Mapper
 public interface ReportMapper extends BaseMapperX<ReportDO> {
+
+    default boolean existsByTable7Id(Long table7Id) {
+        return selectCount(new LambdaQueryWrapperX<ReportDO>()
+                .eq(ReportDO::getTable7Id,
+                        table7Id)) > 0;
+    }
+
+    default boolean existsByTable8Id(Long table8Id) {
+        return selectCount(new LambdaQueryWrapperX<ReportDO>()
+                .eq(ReportDO::getTable7Id,
+                        table8Id)) > 0;
+    }
+
+    default boolean existsByTable9Id(Long table9Id) {
+        return selectCount(new LambdaQueryWrapperX<ReportDO>()
+                .eq(ReportDO::getTable9Id,
+                        table9Id)) > 0;
+    }
+
+    default boolean existsByTable10Id(Long table10Id) {
+        return selectCount(new LambdaQueryWrapperX<ReportDO>()
+                .eq(ReportDO::getTable10Id,
+                        table10Id)) > 0;
+    }
+
+    default boolean existsByTable11Id(Long table11Id) {
+        return selectCount(new LambdaQueryWrapperX<ReportDO>()
+                .eq(ReportDO::getTable11Id,
+                        table11Id)) > 0;
+    }
+
+    default boolean existsByTime(Date beginTime, Date endTime) {
+        return selectCount(new LambdaQueryWrapperX<ReportDO>()
+                .between(ReportDO::getCreateTime,
+                        beginTime,
+                        endTime)) > 0;
+    }
 
     default PageResult<ReportDO> selectPage(ReportPageReqVO reqVO) {
         return selectPage(reqVO, new LambdaQueryWrapperX<ReportDO>()
